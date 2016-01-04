e3e45b2134675017dd746ec760c786c7b3768587
Jackson Roberts <jacksonroberts25@gmail.com>
2016-01-04 16:53:44 -0700

    update

--- TruthBot/TruthBot/settings.py
+++ TruthBot/TruthBot/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'tym-knl)3w)0$1+32v^8cg%9lu8(10ayldt(9rnhr+@4q()pno'
+SECRET_KEY = '89wh2z3^z^-l9=8*j-pcgavps)4jo#(wufgp65)2=yzzv693=a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'login',
 ]
 
 MIDDLEWARE_CLASSES = [
--- TruthBot/auth/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-
-# Create your views here.
-
-def register(request):
-    return HttpResponse("ayy lmao")
