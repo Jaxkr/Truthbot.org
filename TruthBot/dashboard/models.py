3bdd0a2350978a4ffcf2c68f8e9394aa9ee913e7
Jackson Roberts <jacksonroberts25@gmail.com>
2016-01-17 19:09:37 -0700

    further auth

--- TruthBot/dashboard/models.py
+++ TruthBot/dashboard/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class WebPage(models.Model):
+	page_title = models.CharField(max_length=250, blank=False)
+	page_url = models.CharField(max_length=2083, blank=False)
+
+	def __unicode__(self):
+		return self.page_title
