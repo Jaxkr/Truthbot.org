75629a4c7a4e5abc3ae671356c84f5c485bdc8d4
Jackson Roberts <jacksonroberts25@gmail.com>
2016-06-01 16:11:48 -0600

    remove migrations temp

--- Truthbot/dashboard/admin.py
+++ Truthbot/dashboard/admin.py
@@ -3,7 +3,6 @@ from .models import *
 
 # Register your models here.
 
-admin.site.register(WebPage)
 admin.site.register(OrganizationDomain)
 admin.site.register(Organization)
 admin.site.register(LoggedOrganizationEdit)
