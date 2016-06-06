b2101077b6765e533d41c930eae9a29539c047da
Jackson Roberts <jacksonroberts25@gmail.com>
2016-06-06 13:43:21 -0600

    syncup prior to celery

--- Truthbot/extensionapi/views.py
+++ Truthbot/extensionapi/views.py
@@ -18,9 +18,9 @@ def get_site_info(request):
 	parent_organizations = org.parent_organizations.all()
 	parent_organizations_dict = serializers.serialize("python", parent_organizations)
 
+	
 
-
-	data_to_return_dict = {'organization': org_dict, 'first_parent_organizations': parent_organizations_dict}
+	data_to_return_dict = {'organization': org_dict}
 	data_to_return = json.dumps(data_to_return_dict)
 	response = HttpResponse(data_to_return, content_type='application/json')
 	response['Access-Control-Allow-Origin'] = '*'
