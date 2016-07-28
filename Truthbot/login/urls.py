174358d10bd6de2ced36d7112d59e764122839ea
Jackson Roberts <jacksonroberts25@gmail.com>
2016-07-28 11:50:25 -0600

    wow password reset was easy, thanks django

--- Truthbot/login/urls.py
+++ Truthbot/login/urls.py
@@ -1,9 +1,18 @@
 from django.conf.urls import url
+from django.contrib.auth import views as auth_views
 
 from . import views
 
+# just using stock django shit for now
 urlpatterns = [
     url(r'^register/$', views.register, name='register'),
     url(r'^login/$', views.login, name='login'),
-    url(r'^logout/$', views.logout_view, name='logout')
+    url(r'^logout/$', views.logout_view, name='logout'),
+    url(r'^change-password/$',
+        auth_views.password_change, name='password_change'),
+    url(r'^password-change/done/$', auth_views.password_change_done, name='password_change_done'),
+    url(r'^password_reset/$', auth_views.password_reset, name='password_reset'),
+    url(r'^password_reset/done$', auth_views.password_reset_done, name='password_reset_done'),
+    url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', auth_views.password_reset_confirm, name='password_reset_confirm'),
+    url(r'^reset/done/$', auth_views.password_reset_complete, name='password_reset_complete'),
 ]
