1fa811fa55871675c9b6608067078046b672bf6f
Jackson Roberts <jacksonroberts25@gmail.com>
2016-07-30 22:03:47 -0600

    production wsgi, remove this

--- Truthbot/Truthbot/wsgi.py
+++ Truthbot/Truthbot/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Truthbot.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Truthbot.prodsettings")
 
 application = get_wsgi_application()
