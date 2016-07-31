018035f39231b5b3df38622f7625f8d874295452
Jackson Roberts <jacksonroberts25@gmail.com>
2016-07-30 20:10:14 -0600

    some static pages

--- Truthbot/staticpages/urls.py
+++ Truthbot/staticpages/urls.py
@@ -5,4 +5,5 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name="staticpages/index.html"), name='homepage'),
+    url(r'^about/$', TemplateView.as_view(template_name="staticpages/about.html"), name='about'),
 ]
