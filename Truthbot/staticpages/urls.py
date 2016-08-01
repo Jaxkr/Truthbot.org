cfa5c90f49ffe3ffa388f29d5270ab3d37dc14fa
Jackson Roberts <jacksonroberts25@gmail.com>
2016-07-31 18:50:40 -0600

    reddit

--- Truthbot/staticpages/urls.py
+++ Truthbot/staticpages/urls.py
@@ -6,4 +6,5 @@ from django.views.generic import TemplateView
 urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name="staticpages/index.html"), name='homepage'),
     url(r'^about/$', TemplateView.as_view(template_name="staticpages/about.html"), name='about'),
+    url(r'^reddit/$', TemplateView.as_view(template_name="staticpages/reddit.html"), name='reddit'),
 ]
