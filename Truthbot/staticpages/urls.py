7da65062a6e845f615d790ce80e823f8c85c4b84
Jackson Roberts <jacksonroberts25@gmail.com>
2016-08-05 23:32:17 -0600

    updated about page

--- Truthbot/staticpages/urls.py
+++ Truthbot/staticpages/urls.py
@@ -6,5 +6,6 @@ from django.views.generic import TemplateView
 urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name="staticpages/index.html"), name='homepage'),
     url(r'^about/$', TemplateView.as_view(template_name="staticpages/about.html"), name='about'),
+    url(r'^donate/$', TemplateView.as_view(template_name="staticpages/donate.html"), name='donate'),
     url(r'^reddit/$', TemplateView.as_view(template_name="staticpages/reddit.html"), name='reddit'),
 ]
