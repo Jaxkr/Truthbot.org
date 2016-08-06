e616973fece622e19db7dba4df43078dd59c58c1
Jackson Roberts <jacksonroberts25@gmail.com>
2016-08-06 00:01:46 -0600

    added loader key

--- Truthbot/staticpages/urls.py
+++ Truthbot/staticpages/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     url(r'^about/$', TemplateView.as_view(template_name="staticpages/about.html"), name='about'),
     url(r'^donate/$', TemplateView.as_view(template_name="staticpages/donate.html"), name='donate'),
     url(r'^reddit/$', TemplateView.as_view(template_name="staticpages/reddit.html"), name='reddit'),
+    url(r'^loaderio-161269a99b26b52e85511a4334c564af/$', TemplateView.as_view(template_name="staticpages/loader.html")),
 ]
